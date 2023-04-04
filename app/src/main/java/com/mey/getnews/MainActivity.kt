ba3f4cc4fef53fdb27092a87161fb1c2f042136d
Mehmet Yildiz <mhmtxyildiz@gmail.com>
2023-04-04 23:48:42 +0300

    navigation, bottom menu created

--- app/src/main/java/com/mey/getnews/MainActivity.kt
+++ app/src/main/java/com/mey/getnews/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.mey.getnews
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.mey.getnews.databinding.ActivityMainBinding
 import timber.log.Timber
 
@@ -10,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +22,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
+        setupBottomNavigationView()
+
     }
+
+    private fun setupBottomNavigationView() {
+        val navGraph =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navGraph.navController
+        binding!!.bottomNavigationView.setupWithNavController(navController)
+    }
+
 }
